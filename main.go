b35a8603e6fd6da322f993f527a75ea3b2cfb61b
jessegersensonchess <59052632+jessegersensonchess@users.noreply.github.com>
2022-05-27 16:17:12 +0200

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 
 import (
 	"github.com/tidwall/gjson"
-	//	"encoding/json"
 	"bytes"
 	"fmt"
 	"io/ioutil"
@@ -14,82 +13,21 @@ import (
 	"time"
 )
 
-type Chart struct {
-	Result []struct {
-		Meta struct {
-			Currency             string  `json:"currency"`
-			Symbol               string  `json:"symbol"`
-			ExchangeName         string  `json:"exchangeName"`
-			InstrumentType       string  `json:"instrumentType"`
-			FirstTradeDate       int     `json:"firstTradeDate"`
-			RegularMarketTime    int     `json:"regularMarketTime"`
-			Gmtoffset            int     `json:"gmtoffset"`
-			Timezone             string  `json:"timezone"`
-			ExchangeTimezoneName string  `json:"exchangeTimezoneName"`
-			RegularMarketPrice   float64 `json:"regularMarketPrice"`
-			ChartPreviousClose   float64 `json:"chartPreviousClose"`
-			PriceHint            int     `json:"priceHint"`
-			CurrentTradingPeriod struct {
-				Pre struct {
-					Timezone  string `json:"timezone"`
-					End       int    `json:"end"`
-					Start     int    `json:"start"`
-					Gmtoffset int    `json:"gmtoffset"`
-				} `json:"pre"`
-				Regular struct {
-					Timezone  string `json:"timezone"`
-					End       int    `json:"end"`
-					Start     int    `json:"start"`
-					Gmtoffset int    `json:"gmtoffset"`
-				} `json:"regular"`
-				Post struct {
-					Timezone  string `json:"timezone"`
-					End       int    `json:"end"`
-					Start     int    `json:"start"`
-					Gmtoffset int    `json:"gmtoffset"`
-				} `json:"post"`
-			} `json:"currentTradingPeriod"`
-			DataGranularity string   `json:"dataGranularity"`
-			Range           string   `json:"range"`
-			ValidRanges     []string `json:"validRanges"`
-		} `json:"meta"`
-		Timestamp  int `json:"timestamp"`
-		Indicators struct {
-			Quote []struct {
-				Open   []float64 `json:"open"`
-				High   []float64 `json:"high"`
-				Low    []float64 `json:"low"`
-				Close  []float64 `json:"close"`
-				Volume []int     `json:"volume"`
-			} `json:"quote"`
-			Adjclose []struct {
-				Adjclose []float64 `json:"adjclose"`
-			} `json:"adjclose"`
-		} `json:"indicators"`
-	} `json:"result"`
-	Error interface{} `json:"error"`
+func validateCurrencyCode(CurrencyCode string) bool {
+	// description: validates currency code
+	result := false
+	// todo: work up a real test
+	if CurrencyCode == "is in list of currencies" {
+		result = true
+	} else {
+		result = false
+	}
+	return result
 }
 
-func main() {
-	start := time.Now()
-	Qty, _ := 1.0, 0
-
-	if len(os.Args) == 4 {
-		Qty, _ = strconv.ParseFloat(os.Args[3], 64)
-	}
-
-	if len(os.Args) < 3 {
-		fmt.Println("USAGE: con [currency_code] [currency_code] int")
-		fmt.Println("EXAMPLE: con usd czk 100")
-		//		fmt.Println("'CZD' is not a valid currency code. Did you mean")
-		fmt.Println("list of currencies: cat ~/git/scripts/currencies")
-
-		os.Exit(2)
-	}
-
-	CurrencyFrom := strings.ToUpper(os.Args[1])
-	CurrencyTo := strings.ToUpper(os.Args[2])
+func getRate(CurrencyFrom string, CurrencyTo string) float64 {
 	httpposturl := fmt.Sprintf("https://query1.finance.yahoo.com/v7/finance/chart/%v%v%v", CurrencyFrom, CurrencyTo, "=x?corsDomain=finance.yahoo.com&range=1d&interval=1d")
+	// todo: get rid of bytes.NewBuffer yet set a request header
 	var jsonData = []byte(`{
 		"b": "l"
 	}`)
@@ -103,12 +41,50 @@ func main() {
 		panic(error)
 	}
 	defer response.Body.Close()
+	if response.StatusCode != 200 {
+		fmt.Println("something was wrong. status code was not 200")
+		os.Exit(response.StatusCode)
+	}
 	responseData, _ := ioutil.ReadAll(response.Body)
+	// digs into the json and pulls out the value we want
+	// todo: use std library to pull this value using structs
 	value := gjson.GetBytes(responseData, "chart.result.0.meta.regularMarketPrice")
 	regularMarketPrice, _ := strconv.ParseFloat(value.String(), 64)
-	fmt.Println("amount:", Qty, CurrencyFrom, "\n")
-	fmt.Println("1", CurrencyFrom, "=", regularMarketPrice, CurrencyTo)
-	fmt.Printf("1 %v = %.3f %v\n", CurrencyTo, 1/regularMarketPrice, CurrencyFrom)
-	fmt.Println("\ntime taken: ", time.Since(start))
+	return regularMarketPrice
+
+}
+
+func main() {
+	start := time.Now()
+	Qty, _ := 1.0, 0
+	lenArgs := len(os.Args)
+	if lenArgs == 4 {
+		Qty, _ = strconv.ParseFloat(os.Args[3], 64)
+	}
+
+	if lenArgs < 3 {
+		fmt.Println("USAGE: con [currency_code] [currency_code] int")
+		fmt.Println("EXAMPLE: con usd czk 100")
+		fmt.Println("list of currencies: cat ~/git/scripts/currencies")
+		os.Exit(2)
+	}
+
+	CurrencyFrom := strings.ToUpper(os.Args[1])
+	CurrencyTo := strings.ToUpper(os.Args[2])
+	regularMarketPrice := getRate(CurrencyFrom, CurrencyTo)
+	printRates(regularMarketPrice, Qty, CurrencyFrom, CurrencyTo)
+	fmt.Printf("\ntime taken: %v ms", time.Since(start).Milliseconds())
+	printTally(regularMarketPrice, float64(Qty), CurrencyTo)
+
+}
+
+func printTally(regularMarketPrice float64, Qty float64, CurrencyTo string) {
 	fmt.Printf("\n\n  %.2f %v\n\n", regularMarketPrice*float64(Qty), CurrencyTo)
+
+}
+
+func printRates(regularMarketPrice float64, Qty float64, CurrencyFrom string, CurrencyTo string) {
+	fmt.Printf("amount: %v %v\n", Qty, CurrencyFrom)
+	fmt.Printf("1 %v = %v %v\n", CurrencyFrom, regularMarketPrice, CurrencyTo)
+	fmt.Printf("1 %v = %.3f %v\n", CurrencyTo, 1/regularMarketPrice, CurrencyFrom)
 }
