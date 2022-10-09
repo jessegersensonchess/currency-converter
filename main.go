d17a64e5eaa779d60fc0dcc2ab7d9344c961a0ae
jessegersensonchess <59052632+jessegersensonchess@users.noreply.github.com>
2022-10-10 00:19:18 +0200

    Update main.go

--- main.go
+++ main.go
@@ -28,7 +28,6 @@ func redisSet(key string, val string) {
 	})
 	err := Rdb.Set(ctx, key, val, redisTTL*time.Second).Err()
 	if err != nil {
-		fmt.Println("redis set error")
 	}
 	return
 }
@@ -58,53 +57,10 @@ type apiResponse struct {
 			Meta struct {
 				Currency             string  `json:"currency"`
 				Symbol               string  `json:"symbol"`
-				ExchangeName         string  `json:"exchangeName"`
-				InstrumentType       string  `json:"instrumentType"`
-				FirstTradeDate       int     `json:"firstTradeDate"`
 				RegularMarketTime    int     `json:"regularMarketTime"`
-				Gmtoffset            int     `json:"gmtoffset"`
-				Timezone             string  `json:"timezone"`
-				ExchangeTimezoneName string  `json:"exchangeTimezoneName"`
 				RegularMarketPrice   float64 `json:"regularMarketPrice"`
-				ChartPreviousClose   float64 `json:"chartPreviousClose"`
-				PriceHint            int     `json:"priceHint"`
-				CurrentTradingPeriod struct {
-					Pre struct {
-						Timezone  string `json:"timezone"`
-						Start     int    `json:"start"`
-						End       int    `json:"end"`
-						Gmtoffset int    `json:"gmtoffset"`
-					} `json:"pre"`
-					Regular struct {
-						Timezone  string `json:"timezone"`
-						Start     int    `json:"start"`
-						End       int    `json:"end"`
-						Gmtoffset int    `json:"gmtoffset"`
-					} `json:"regular"`
-					Post struct {
-						Timezone  string `json:"timezone"`
-						Start     int    `json:"start"`
-						End       int    `json:"end"`
-						Gmtoffset int    `json:"gmtoffset"`
-					} `json:"post"`
-				} `json:"currentTradingPeriod"`
-				DataGranularity string   `json:"dataGranularity"`
-				Range           string   `json:"range"`
-				ValidRanges     []string `json:"validRanges"`
 			} `json:"meta"`
 			Timestamp  []int `json:"timestamp"`
-			Indicators struct {
-				Quote []struct {
-					High   []float64 `json:"high"`
-					Volume []int     `json:"volume"`
-					Open   []float64 `json:"open"`
-					Low    []float64 `json:"low"`
-					Close  []float64 `json:"close"`
-				} `json:"quote"`
-				Adjclose []struct {
-					Adjclose []float64 `json:"adjclose"`
-				} `json:"adjclose"`
-			} `json:"indicators"`
 		} `json:"result"`
 		Error interface{} `json:"error"`
 	} `json:"chart"`
